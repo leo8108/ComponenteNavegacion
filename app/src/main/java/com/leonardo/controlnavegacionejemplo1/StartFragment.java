861a49ce40aafcc30eb425379eb55535be1c2a04
Leonardo Manzano Salazar <leonardo.manzano@outlook.es>
2021-01-26 12:06:46 -0600

    Ejercicios 1 de navegation component

---
+++ app/src/main/java/com/leonardo/controlnavegacionejemplo1/StartFragment.java
@@ -0,0 +1,49 @@
+package com.leonardo.controlnavegacionejemplo1;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link StartFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class StartFragment extends Fragment {
+
+
+    public StartFragment() {
+        // Required empty public constructor
+    }
+    
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        NavController navController = Navigation.findNavController(view);
+        Button button= view.findViewById(R.id.startGameBoton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_startFragment_to_gameFragment);
+            }
+        });
+    }
+}
